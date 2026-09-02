2081468a446f07494015c76eff1b10881b8d872b
shashika-mora <shashikatheekshana67@gmail.com>
2026-09-02 20:32:46 +0530

    feat(backend): initialize Spring Boot project structure and dependencies
    ## Summary
    
    Set up core Spring Boot application structure, configuration, and Maven dependencies.
    
    ## Key Additions
    
    - pom.xml: Configure dependencies for Spring Boot 3, Spring Web, Spring JDBC, MySQL Connector, and Lombok.
    
    - src/: Add main application class, application properties, and initial test setup.

---
+++ src/main/java/com/datanexus/catms/CatmsApplication.java
@@ -0,0 +1,13 @@
+package com.datanexus.catms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datanexus/catms/CatmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datanexus.catms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
